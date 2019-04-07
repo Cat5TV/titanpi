02799ccd20c01e8a20bd247a9ed8572d4c46d3c7
Robbie Ferguson <robbie@category5.tv>
2019-04-07 14:49:15 -0400

    Begin setting up prerequisites

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [[ ! -d Titan ]]; then
+  echo "Titan is missing. Did you clone this repo with --recursive ?"
+  exit 1
+fi
+basedir=$(pwd)
+
+apt -y install build-essential checkinstall
+apt -y install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+apt -y install python
+
+cd /tmp
+mkdir python
+cd pyton
+wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
+tar xzf Python-3.7.3.tgz
+cd Python-3.7.3
+./configure --enable-optimizations
+make altinstall
+
+cd $basedir
+cd Titan
+pip install -r requirements.txt
